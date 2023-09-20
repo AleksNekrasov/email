514b9f97727b179e6f1518bd9f0b4dde3a95e0f4
Grip <nsk540010@gmail.com>
2023-09-20 22:34:20 +0700

    Законченная полностью рабочая программа

--- email/email.cpp
+++ email/email.cpp
@@ -32,32 +32,43 @@ int main()
 int function1(std::string email)
 {
     //создаем словарь
-    // первый символ ТОЧКА. 
+     
     std::string check = ".-@0123456789abcdefghijklmnopqrstuvwxyz!#$%&'*+-/=?^_`{|}~";
 
+    int flag =0; // флажок поиска недопустимых переменных
+    int j = 0;
     for (int i = 0; i < email.length(); i++)
     {
         
-        for (int j = 0; j < check.length(); j++)
+        for (; j < check.length(); j++)
         {
-            if (email[0] == check[0]) return -1;  // если первая точка
+            if (email[0] == '.') return -1;  // если первая точка
+
             if (i > 64) return -1;  //если первая половина до @ больше 64
+
             if (i > 0)
             {
-                if (email[i] == check[0] && email[i - 1] == check[0])
+                if (email[i] == '.' && email[i - 1] == '.')
                     return -1; // если две точки подряд
             }
 
-            //if (email[i] != check[check.length() - 1]) return -1; // если после прогона цикла по словарю не нашли ни одного совпадения
-
             //единственно правильный вариант
+            if (email[i] == '@' && email[i - 1] != '.') return i; // если мы находим собачку '@' И перед ней нет точки(по условию)  выходим и возвращаем значение i
+
+            if (email[i] == check[j]) // если мы находим совпадение
+            {
+                j = 0;
+                flag++;  // проверка на недопустимые символы
+                break;
+            }
             
-            if (email[i] == '@' && email[i - 1] != '.') return i;
-            if (email[i] == check[j]) break;
+            
+            
         }
-       // if (email[i] != check[check.length() - 1]) return -1; // если после прогона цикла по словарю не нашли ни одного совпадения
+        if (flag -1 != i) return -1; // проверка на недопустимые символы
         
     }
+    
     return -1;
 }
 
@@ -67,23 +78,34 @@ int function1(std::string email)
 int function2(int  fi, std::string email) // передаем значение i , и наш емеил
 {
     //создаем словарь
-   // первый символ ТОЧКА. 
+    
     std::string check = ".-0123456789abcdefghijklmnopqrstuvwxyz";
 
     int i = fi + 1;// создаем новый счетчик
     if (i <= 1) return -1;  // проверка на смысл проверять вторую часть
 
+    if (email[i] == '.') return -1;// проверка на первую точку '.'  после знака @
+
+    int flag = 0; // флажок поиска недопустимых переменных
+
     for (; i < email.length(); i++)
     {
-        for (int j = 0; j < check.length(); j++)
+        int j = 0;
+        for (; j < check.length(); j++)
         {
-            //if (email[0] == check[0]) return -1;  // если первая точка
-            if ((email.length() - (i + 1)) > 63) return -1;                //если вторая половина после @ больше 63                               
-            if (email[i] == check[0] && email[i - 1] == check[0]) return -1; // если две точки подряд
-            //if (email[i] != check[check.length() - 1]) return -1; // если после прогона цикла по словарю не нашли ни одного совпадения   НУЖНО ДОРАБОТАТЬ !!!!
-            if (email[i] == check[j]) break;
-        }
+            if ((email.length() - (i + 1)) > 63) return -1;                //если вторая половина после @ больше 63 
 
+            if (email[i] == '.' && email[i - 1] == '.') return -1; // если две точки подряд
+            
+            if (email[i] == check[j]) // если совпадение найдено
+            {
+                j = 0;
+                flag++; // проверка на недопустимые символы..
+                break;
+            }
+            
+        }
+        if (flag  + fi != i) return -1; // проверка на недопустимые символы..
     }
     return 1;
 }
